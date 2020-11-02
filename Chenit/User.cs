6bea49f7886e79308d3d3ec69e0ea46e4c1c5e15
Alex <sasha13223@mail.ru>
2020-11-02 21:24:39 +0300

    For fun

---
+++ Chenit/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chenit
+{
+    class User
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
