7716bade88e941db3921a4771c37458e4b24355b
Alex <sasha13223@mail.ru>
2020-11-02 21:26:06 +0300

    XZ

--- Chenit/Creator.cs
+++ Chenit/Creator.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace Chenit
 {
-    class User
+    class Creator
     {
+        public User Users { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
     }
